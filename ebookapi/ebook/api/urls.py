ae11898a4410b3e005008225e1008de8c154b62f
dh <dhkim12310@gmail.com>
2020-09-23 13:10:15 +0900

    리뷰 api 구현

--- ebookapi/ebook/api/urls.py
+++ ebookapi/ebook/api/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from ebook.api.views import EbookListAPIView,EbookDetailAPIView
+from ebook.api.views import (EbookListAPIView,
+                             EbookDetailAPIView,
+                             ReviewCreateAPIView,
+                             ReviewDetailAPIView)
 
 urlpatterns = [
     path('ebook/',EbookListAPIView.as_view(),name='ebook-list'),
-    path('ebook/<int:pk>/',EbookDetailAPIView.as_view(),name='ebook-detail')
+    path('ebook/<int:pk>/',EbookDetailAPIView.as_view(),name='ebook-detail'),
+    path('ebook/<int:ebook_pk>/review/',ReviewCreateAPIView.as_view(),name='ebook-review'),
+    path('review/<int:pk>',ReviewDetailAPIView.as_view(),name='review-datail')
 ]
--- ebookapi/ebook/api/views.py
+++ ebookapi/ebook/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.generics import get_object_or_404
 from rest_framework import mixins
 
 from ebook.models          import Ebook, Review
@@ -11,3 +12,16 @@ class EbookListAPIView(generics.ListCreateAPIView):
 class EbookDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Ebook.objects.all()
     serializer_class = EbookSerializer
+
+class ReviewCreateAPIView(generics.CreateAPIView):
+    querset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    def perform_create(self,serializer):
+        ebook_pk = self.kwargs.get("ebook_pk")
+        ebook = get_object_or_404(Ebook, pk=ebook_pk)
+        serializer.save(ebook=ebook)
+
+class ReviewDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
